493f7930d42bf7b0b06e07f5599ac764f19989a5
Farhan933 <farhanfarabiemon@gmail.com>
2023-11-24 17:03:26 +0600

    404 page

---
+++ src/pages/ErrorPage/ErrorPage.jsx
@@ -0,0 +1,29 @@
+import Lottie from 'lottie-react';
+import errorPage from '../../assets/lottieAnimation/404.json'
+import { Button } from '@mui/material';
+import HomeIcon from '@mui/icons-material/Home';
+import { useNavigate } from "react-router-dom";
+import { purple } from '@mui/material/colors';
+import Typography from '@mui/material/Typography';
+import Box from '@mui/material/Box';
+
+const ErrorPage = () => {
+    const navigate = useNavigate();
+
+    const handlehome = () => {
+        navigate('/');
+    }
+
+    return (
+        <div className="h-screen flex flex-col items-center justify-center">
+            <Lottie animationData={errorPage} loop={true}></Lottie>
+            <Typography component="div">
+                <Box sx={{ letterSpacing: 4, m: 1 }}>
+                    <h2 className='heading-font text-xl' >I am very sleepy,please go <Button onClick={handlehome} variant="contained" endIcon={<HomeIcon></HomeIcon>} style={{ background: purple[500] }} all-font>Home</Button></h2>
+                </Box>
+            </Typography>
+        </div>
+    );
+};
+
+export default ErrorPage;
